04d12db1a17659c8003c17c0dcb58b2ba1f87132
Nur tusa <nur.tusa@student.moringaschool.com>
2021-09-06 02:17:18 -0400

    fixing js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function AKAN_NAME() {
-    var dd = document.getElementById("dd").value; // date
+    var dy = document.getElementById("dy").value; // date
 
-    var mm = document.getElementById("mm").value; //month
+    var mth = document.getElementById("mth").value; //month
 
-    var yy = document.getElementById("yy").value; //year
+    var yr = document.getElementById("yr").value; //year
 
     var female = document.getElementById("female").checked; // female
 
@@ -39,42 +39,42 @@ function AKAN_NAME() {
 
     /////MY FORMULA
 
-    var birthDate = new Date(mm + '/' + dd + '/' + yy);   ////////  putting birthday as a new date
+    var birthD = new Date(mth + '/' + dy + '/' + yr);   ////////  putting birthday as a new date
 
-    var dayOfTheWeek = birthDate.getDay();
+    var dayWeek = birthD.getDay();
     //////////////////if enterd in valid date 
     
-    if (dd <= 0 || dd > 31) {
+    if (dy <= 0 || dy > 31) {
         alert("Please enter a valid date!");
 
-    } else if ((mm === "September" || mm === "April" || mm === "June" || mm === "November") && (dd <= 0 || dd > 30)) {
+    } else if ((mth === "September" || mth === "April" || mth === "June" || mth === "November") && (dy <= 0 || dy > 30)) {
         alert("Please enter a valid date!");
         /////////if left black sector of month 
 
-    } else if (mm === "------") {
+    } else if (mth === "------") {
         alert("Please enter a valid date!");
 
-    } else if (yy <= 0 ) {
+    } else if (yr <= 0 ) {
         alert("Please enter a valid date!");
 
         ////////////leap year
 
-    } else if ((mm === "February") && (dd <= 0 || dd > 29) && (0 == yy % 4)) {
+    } else if ((mth === "February") && (dy <= 0 || dy > 29) && (0 == yr % 4)) {
         alert("Please enter a valid date!");
 
         //////////not leap year 
-    } else if ((mm === "February") && (dd <= 0 || dd > 28) && (0 != yy % 4)) {
+    } else if ((mth === "February") && (dy <= 0 || dy > 28) && (0 != yr % 4)) {
         alert("Please enter a valid date!");
 
 
         ////////////// respond for female
     } else if (female === true) {
-        alert("You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + femaleNames[dayOfTheWeek] + "!"); //shows result by replacing the HTML content in the id=result
+        alert("You were born on a " + days[dayWeek] + ".\n" + "Your Akan name is " + femaleNames[dayWeek] + "!"); //shows result by replacing the HTML content in the id=result
        
         /////////////repond for male
 
     } else if (male === true) {
-        alert("You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + maleNames[dayOfTheWeek] + "!"); //shows result by replacing the  HTML content in the id=result
+        alert("You were born on a " + days[dayWeek] + ".\n" + "Your Akan name is " + maleNames[dayWeek] + "!"); //shows result by replacing the  HTML content in the id=result
        
 
         ////////////////if not selected any button
